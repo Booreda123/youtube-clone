ea8f0aea0f2b4e0fc755329b15fcbf48228645b2
Booreda123 <booreda58@gmail.com>
2023-01-08 13:59:37 +0100

    fc

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/youtube-clone/' element={<Feed />} />
-        <Route path='video/:id' element={<VideoDetail />} />
-        <Route path='channel/:id' element={<ChannelDetail />} />
-        <Route path='search/:searchTerm' element={<SearchFeed />} />
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
